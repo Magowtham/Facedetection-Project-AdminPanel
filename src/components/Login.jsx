2d0a8b580cab5ba844d4aa708e7489629b53c5be
Gowtham MA <magowtham7@gmail.com>
2024-02-13 13:46:14 +0530

    initiated login page ui

---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../css/Login.css"
+
+function Login() {
+  return (
+    <>
+        <div>
+            <input type="text"/>
+            <input/>
+            <button>Login</button>
+        </div>
+    </>
+  )
+}
+
+export default Login;
